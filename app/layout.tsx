9eb054112201c00ccd2fe74feca415f32b00eaa5
Wesley Sum <wesleysum0402@vt.edu>
2024-08-13 15:57:21 -0400

    Integrated posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,21 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import posthog from "posthog-js";
+import { PHProvider } from "./providers";
+import dynamic from "next/dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Wesley | Personal Portfolio",
-  description: "Wesley is a software engineer with a passion for building things.",
+  description:
+    "Wesley is a software engineer with a passion for building things.",
 };
+
+const PostHogPageView = dynamic(() => import("./PostHogPageView"), {
+  ssr: false,
+});
 
 export default function RootLayout({
   children,
@@ -21,23 +29,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body
-        className={`${inter.className} bg-gray-50 text-gray-850 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
-      >
-        <div className="bg-teal-300 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#00BFFF]"></div>
-        <div className="bg-blue-300 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#00024D]"></div>
+      <PHProvider>
+        <body
+          className={`${inter.className} bg-gray-50 text-gray-850 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+        >
+          <div className="bg-teal-300 absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#00BFFF]"></div>
+          <div className="bg-blue-300 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#00024D]"></div>
 
-        <ThemeContextProvider>
-          <ActiveSectionContextProvider>
-            <Header />
-            {children}
-            <Footer />
+          <ThemeContextProvider>
+            <ActiveSectionContextProvider>
+              <Header />
+              <PostHogPageView />
+              {children}
+              <Footer />
 
-            <Toaster position="top-right" />
-            <ThemeSwitch />
-          </ActiveSectionContextProvider>
-        </ThemeContextProvider>
-      </body>
+              <Toaster position="top-right" />
+              <ThemeSwitch />
+            </ActiveSectionContextProvider>
+          </ThemeContextProvider>
+        </body>
+      </PHProvider>
     </html>
   );
 }
